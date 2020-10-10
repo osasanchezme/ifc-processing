8850706c3626164242b83b8feb6666f55ca95945
Oscar <osasanchezme@unal.edu.co>
2020-10-10 09:37:27 -0500

    Able to process beam and columns of a ifc file
    Can export the 3d and idealized but disjoined beams and columns of the given file
    
    Future work:
    - Join beams and columns of the idealized model
    - Prepare data to export it to a JSON file readable by FEM.js

--- IFCtoSCR.py
+++ IFCtoSCR.py
@@ -2,58 +2,37 @@
 
 import ifcopenshell
 from ifcopenshell import geom
+import re
 
 
-def read_geom(ifc_path, ObjType):
+def read_geom(ifc_path, ObjTypes):
     """Function to read elements from a ifc file
 
     Args:
         ifc_path (str): Full or relative path of the ifc file
-        ObjType (str): IfcType of the elements want to get back
+        ObjTypes (list): IfcTypes of the elements want to get info back
 
     Returns:
-        dict: Dictionary containing the vertices, edges and face of each element, using the globally unique id
+        dict: Dictionary containing dictionaries with the vertices, edges and tag of each element, using the globally unique id
     """
     ifc_file = ifcopenshell.open(ifc_path)
     settings = geom.settings()
     settings.set(settings.USE_WORLD_COORDS, True)
-    ios_vertices = {}
-    ios_edges = {}
-    ios_faces = {}
-    ios_elements = {}
+    objects_geometry = {}
 
-    for ifc_entity in ifc_file.by_type(ObjType): # Retrieves all the elements of the given type
-        # print(ifc_entity[8]) # From 0 up to 8
-        # print(ifc_entity) # Gives the ifc file line of the element
-        # print(ifc_entity[0]) # Gives the globally unique universal id of the element
-        # print(ifc_entity[7]) # Gives the tag/Revit ID of the element
-        shape = geom.create_shape(settings, ifc_entity) # Convert the ifc into a mesh
-        # ios stands for IfcOpenShell
-        ios_vertices[ifc_entity[0]] = shape.geometry.verts
-        ios_edges[ifc_entity[0]] = shape.geometry.edges
-        ios_faces[ifc_entity[0]] = shape.geometry.faces
-        ios_elements[ifc_entity[0]] = ifc_entity[7]
+    for ObjType in ObjTypes:
+        ios_vertices = {}
+        ios_edges = {}
+        ios_elements = {}
 
-    return {"vertices": ios_vertices, "edges": ios_edges, "faces": ios_faces, "elements": ios_elements}
-
-def writePoints(vertices, file_name):
-    """Function to write the points into a scr file
-
-    Args:
-        vertices (list): list containing a tuple with (x,y,z) coords for each vertex
-        file_name (str): name of the file to be written with the autocad points
-    """
-    with open(file_name, "w") as f:
-        f.write("zoom -100,-100,-100 -200,-200,-200\n")
-        for point in vertices:
-            p_str = str(point).replace(" ", "")
-            p_str = f"point {p_str}\n"
-            p_str = p_str.replace("(", "")
-            p_str = p_str.replace(")", "")
-            f.write(p_str)
-        f.write("zoom a\n")
-        f.close()
-    print(f"Successfully exported the points to: {file_name}")
+        for ifc_entity in ifc_file.by_type(ObjType): # Retrieves all the elements of the given type
+            shape = geom.create_shape(settings, ifc_entity) # Convert the ifc into a mesh
+            ios_vertices[ifc_entity[0]] = shape.geometry.verts
+            ios_edges[ifc_entity[0]] = shape.geometry.edges
+            ios_elements[ifc_entity[0]] = ifc_entity[7]
+        objects_geometry[ObjType] = {"vertices": ios_vertices, "edges": ios_edges , "elements": ios_elements}
+    
+    return objects_geometry
 
 def writeLines(lines, file_name, mode = "w"):
     """Function to write the lines into a scr file
@@ -122,7 +101,6 @@ def idealizeColumn(elementLines):
         x_avg /= len(lines)*2
         y_avg /= len(lines)*2
         z_avg /= len(lines)*2
-        print(len(lines))
         return (x_avg, y_avg, z_avg)
     zs = []
     interestingLines = []
@@ -143,33 +121,50 @@ def idealizeColumn(elementLines):
             bottomLines.append(line)
     return (calcAvgPoint(bottomLines), calcAvgPoint(topLines))
 
-def process_beams_columns(input_ifc_file_path, element_type, output_elem_filename, output_ideal_filename):
-    ifc_values = read_geom(input_ifc_file_path, element_type)
-    vertices, edges, faces, elements = ifc_values['vertices'], ifc_values['edges'], ifc_values['faces'], ifc_values['elements']
-    f = open(output_elem_filename, "w")
-    f.close()
-    f = open(output_ideal_filename, "w")
-    f.close()
+def abstract_elements_by_type(ifc_values):
+    """Converts ifcOpenShell's returned objects into separated tuples containing the lines for each element
+
+    Args:
+        ifc_values (dict): Dictionary with points, edges and tags for each element.
+
+    Returns:
+        dict: Dictionary with lines for each element of the given type. key: UUID: XXXX Tag: XXXX
+    """
+    vertices, edges, elements = ifc_values['vertices'], ifc_values['edges'], ifc_values['elements']
+    lines_for_type = {}
     for key in elements: # Loop through the keys of each element
-        print(f"\n\n----------------Element with UUID: {key} and tag: {elements[key]}----------------")
         # Preprocessing
         points = [ vertices[key][i*3:i*3+3] for i in range(len(vertices[key])//3) ]
         line_points = [ edges[key][i*2:i*2+2] for i in range(len(edges[key])//2) ] #List containing a tuple for each line with start and end points id
         line_points = list(set(line_points)) # The line vertices pointers are duplicated, delete duplicates
         lines = [ (points[line_points[i][0]], points[line_points[i][1]]) for i in range(len(line_points)) ] # List of tuples containing the xyz coords of the start and end point
-        # writeLines(lines, f"Line_{key}.scr") # To write lines for a single element - Don't use
+        lines_for_type["UUID: " + str(key) + " Tag:" + str(elements[key])] = lines
 
+    return lines_for_type
+
+def process_file(input_ifc_file_path, output_elem_filename, output_ideal_filename):
+    objectTypes = ["IfcBeam", "IfcColumn"]
+    geometry = read_geom(input_ifc_file_path, objectTypes)
+    ifc_beams = geometry["IfcBeam"]
+    ifc_columns = geometry["IfcColumn"]
+    beams = abstract_elements_by_type(ifc_beams)
+    columns = abstract_elements_by_type(ifc_columns)
+
+    f = open(output_elem_filename, "w")
+    f.close()
+    f = open(output_ideal_filename, "w")
+    f.close()
+
+    for key in beams: # Loop through the keys of each element
+        print(f"\n\n---------------- {key} ----------------")
         # Exporting
-        # writeLines(lines, elementsFile, "a") # To write lines for all the elements
-        if element_type=="IfcBeam":
-            writeLines(lines, output_elem_filename, "a")
-            idealBeam = [idealizeBeam(lines)] # Pass an element as an argument and gives back a line
-            writeLines(idealBeam, output_ideal_filename, "a")
-        elif element_type=="IfcColumn":
-            writeLines(lines, output_elem_filename, "a")
-            idealColumn = [idealizeColumn(lines)]
-            writeLines(idealColumn, output_ideal_filename, "a")
+        writeLines(beams[key], output_elem_filename, "a")
+        idealBeam = [idealizeBeam(beams[key])] # Pass an element as an argument and gives back a line
+        writeLines(idealBeam, output_ideal_filename, "a")
+    for key in columns:
+        writeLines(columns[key], output_elem_filename, "a")
+        idealColumn = [idealizeColumn(columns[key])]
+        writeLines(idealColumn, output_ideal_filename, "a")
 
 if __name__ == "__main__":
-    process_beams_columns("../Example2.ifc", "IfcBeam", "3dBeams.scr","IdealBeams.scr")
-    process_beams_columns("../Example2.ifc", "IfcColumn", "3dColumns.scr","IdealColumns.scr")
+    process_file("../Example2.ifc", "3dModel.scr","IdealModel.scr")
